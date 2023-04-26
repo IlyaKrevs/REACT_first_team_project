88764c99f09810a5c66975cdcba88b025fc75456
Ilya <m-u-r-z-a@mail.ru>
2023-04-26 22:04:38 +0500

    day 11 finish

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,45 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 
 import Carousel from './Components/UI/Carousel';
+import Gallery from './Components/Gallery/Gallery';
+import BigButtonPROMO from './Components/BigButtonPROMO/BigButtonPROMO';
+import HomePageClause from './Components/HomePageClause/HomePageClause';
+
+import bigPhoto from './img/bigSLIDERPHOTO.jpg'
 
 function App() {
   return (
     <div className="App">
 
-
-      <Carousel />
-
-
-
       <Header />
       <Footer />
+      <Gallery />
+      <Carousel />
+      <BigButtonPROMO />
 
 
+      <HomePageClause />
 
 
+      <div className="bigSlider">
+        <div className="bigSliderContainer">
+
+
+          <div className="bigSlider__carouselItem">
+            <img className='bigSlider__IMAGE' src={bigPhoto} alt="bigPhoto" />
+
+            <div className='bigSlider__carouselCONTENT'>
+              <div className='bigSlider__carouselItem__title'>Создано Иви</div>
+              <div className='bigSlider__carouselItem__sinopsys'>Целый вагон впечатлений и тележка эмоций: все проекты производства Иви в одном месте. Запрыгивай!</div>
+            </div>
+            <div className='bigSliderButtonContainer'>
+              <div className='subscribeButton'>
+                <p>Оплатить подписку</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Gallery/Gallery.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div className='gallery_blockHeader'>
+            <div className='gallery__blockHeader__text'>
+                Лучшие сериалы в подписке
+            </div>
+            <div className='gallery__blockHeader__arrow'>
+                <div className='carousel__arrowContainer miniArrow'></div>
+            </div>
+        </div>
+    );
+};
+
+export default Gallery;
